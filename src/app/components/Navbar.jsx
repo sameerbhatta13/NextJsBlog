b9a85c629f1f2d91b54aae24bde38e50ea05d495
sameer bhatta <sameerbhatta37@gmail.com>
2025-03-04 17:35:22 +0545

    first add

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import React from 'react'
+
+
+const Navbar = () => {
+    return (
+        <>
+            <div className='bg-gray-400 flex flex-row mx-20 py-2 gap-6'>
+                <div className='flex '>
+                    <Link href='' className='mx-2'>LOGO</Link>
+                </div>
+
+                <div className='flex justify-end'>
+                    <div>
+                        <Link href='' className='mx-2'>login</Link>
+                        <Link href='' className='mx-2'>signup</Link>
+                        <Link href='' className='mx-2'>profile</Link>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </>
+    )
+}
+
+export default Navbar
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,10 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+        <Navbar />
         {children}
+
       </body>
     </html>
   );
