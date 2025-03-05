28d42d11e0e3ff2c1fadad5199373a133e0073a9
sameer bhatta <sameerbhatta37@gmail.com>
2025-03-05 16:43:17 +0545

    change in home page

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,25 +1,32 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const Navbar = () => {
-    return (
-        <>
-            <div className='bg-gray-400 flex flex-row mx-20 py-2 gap-6'>
-                <div className='flex '>
-                    <Link href='' className='mx-2'>LOGO</Link>
-                </div>
+    const [] = useState()
 
-                <div className='flex justify-evenly'>
-                    <div>
-                        <Link href='' className='mx-2'>login</Link>
-                        <Link href='' className='mx-2'>signup</Link>
-                        <Link href='' className='mx-2'>profile</Link>
+    return (
+
+        <>
+
+            <div className='bg-gray-100' >
+                <div className='bg-[#B6AAA2] flex flex-row justify-between mx-20 py-5 mt-1 gap-6 font-serif text-xl'>
+                    <div className='flex '>
+                        <Link href='' className='mx-2'>LOGO</Link>
+                    </div>
+
+                    <div className='flex gap-6 mx-9'>
+                        <div>
+                            <Link href='' className='mx-2'>login</Link>
+                            <Link href='' className='mx-2'>signup</Link>
+                            <Link href='' className='mx-2'>profile</Link>
+
+                        </div>
 
                     </div>
 
                 </div>
-
             </div>
 
         </>
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,29 @@
+
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+
 
 export default function Home() {
   return (
     <>
       {/* <Navbar /> */}
-      <h1>welcome to the home page</h1>
+      <div>
+
+        <div className="bg-[#CBCBCB] mx-20 my-3">
+          <div className="grid grid-cols-4">
+            <div className=" bg-red-300 grid col-span-2 p-2 m-3 ">
+              <h1>
+                this is first block
+              </h1>
+            </div>
+            <div className="bg-green-300 grid col-span-2 p-2 m-3">
+              <h1>this is second block</h1>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
     </>
   );
 }
