c0d7c4884864d1107d8e97dc82fc6c02f6baa8ba
sameer bhatta <sameerbhatta37@gmail.com>
2025-12-30 16:33:24 +0545

    updated the css

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-            <div className='flex justify-around flex-col gap-2 py-4 sm:gap-0 sm:flex-row bg-gray-600'>
+            <div className='flex justify-around flex-col gap-3 py-4 sm:gap-0 sm:flex-row bg-gray-600'>
                 <Image src='/moon-removebg-preview.png' alt='footer img' width={50} height={50} />
                 <p className='text-sm text-white'>All right reserved. Copyright @Moon</p>
 
